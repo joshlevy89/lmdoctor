312cd8ef6e7dadd3d17b2beccd9ebb58d89e08a2
root <joshlevy89@gmail.com>
2024-03-21 13:29:25 +0000

    got rid of example_utils.py

--- examples/example_utils.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-def load_model(model_name_or_path, revision, device):
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path, device_map=device, revision=revision, trust_remote_code=False)
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True, padding_side="left")
-    tokenizer.pad_token_id = 0
-    return model, tokenizer
