d1168576b915d63c53f532ba5d190395f0bcf9df
root <joshlevy89@gmail.com>
2024-04-25 17:30:38 +0000

    Finished subjective content and coding/reasoning

--- lmdoctor/plot_utils.py
+++ lmdoctor/plot_utils.py
@@ -2,19 +2,22 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-def plot_projection_heatmap(all_projs, tokens, lastn_tokens_to_plot=None, saturate_at=3, figsize=(1000,600)):
+def plot_projection_heatmap(all_projs, tokens, lastn_tokens_to_plot=None, saturate_at=3, figsize=(1000,600), aspect=None):
     """
     Projections by token/layer
     saturate_at ensures that large values don't dominate and can be adjusted. To get raw view, set to None.
     """
+    plot_tokens = None
     if lastn_tokens_to_plot:
         plot_data = all_projs[:, -lastn_tokens_to_plot:].cpu().numpy()
-        plot_tokens = tokens[-lastn_tokens_to_plot:]
+        if tokens:
+            plot_tokens = tokens[-lastn_tokens_to_plot:]
     else:
         plot_data = all_projs.cpu().numpy()
-        plot_tokens = tokens
+        if tokens:
+            plot_tokens = tokens
     
-    fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"))
+    fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"), aspect=aspect)
     if saturate_at is not None:
         if saturate_at == -1:
             # set the max and min based on largest value in data
@@ -23,11 +26,11 @@ def plot_projection_heatmap(all_projs, tokens, lastn_tokens_to_plot=None, satura
         else:
             fig.update_coloraxes(cmin=-saturate_at, cmax=saturate_at)
         
-    
-    fig.update_xaxes(
-        tickvals=list(range(len(plot_tokens))),
-        ticktext=plot_tokens
-    )
+    if tokens:
+        fig.update_xaxes(
+            tickvals=list(range(len(plot_tokens))),
+            ticktext=plot_tokens
+        )
     
     fig.update_layout(
         width=figsize[0],
@@ -38,18 +41,22 @@ def plot_projection_heatmap(all_projs, tokens, lastn_tokens_to_plot=None, satura
     
 
 
-def plot_scores_per_token(readings, tokens, lastn_tokens_to_plot=None, detection_method=None, saturate_at=1):
+def plot_scores_per_token(
+    readings, tokens, lastn_tokens_to_plot=None, detection_method=None, saturate_at=1, figsize=None, aspect=None):
     """
     Scores (e.g. lie detection scores) per token.
     """
+    plot_tokens = None
     if lastn_tokens_to_plot:
         plot_data = readings[:, -lastn_tokens_to_plot:]
-        plot_tokens = tokens[-lastn_tokens_to_plot:]
+        if tokens:
+            plot_tokens = tokens[-lastn_tokens_to_plot:]
     else:
         plot_data = readings
-        plot_tokens = tokens
-    
-    fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"))
+        if tokens:
+            plot_tokens = tokens
+
+    fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"), aspect=aspect)
 
     if detection_method == 'classifier':
         fig.update_coloraxes(cmin=0, cmax=1)
@@ -61,18 +68,28 @@ def plot_scores_per_token(readings, tokens, lastn_tokens_to_plot=None, detection
                 fig.update_coloraxes(cmin=-absmax, cmax=absmax)
             else:
                 fig.update_coloraxes(cmin=-saturate_at, cmax=saturate_at)
-    
-    fig.update_xaxes(
-        tickvals=list(range(len(plot_tokens))),
-        ticktext=plot_tokens,
-        tickangle=-45,  # Tilts the labels at -45 degrees
-        tickfont=dict(size=20)  # Updates the font size to 12
-    )
+
+
+    if tokens:
+        fig.update_xaxes(
+            tickvals=list(range(len(plot_tokens))),
+            ticktext=plot_tokens,
+            tickangle=-45,  # Tilts the labels at -45 degrees
+            tickfont=dict(size=20)  # Updates the font size to 12
+        )
 
     fig.update_yaxes(
         showticklabels=False
     )
 
+
+    if figsize:
+        fig.update_layout(
+            autosize=False,
+            width=figsize[0],  # or any other width
+            height=figsize[1]   # adjust the height accordingly
+        )
+        
     fig.show()
 
 
