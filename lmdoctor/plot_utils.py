7e22f86975d45c9b32c3fae88a7810a9c2d850c2
root <joshlevy89@gmail.com>
2024-05-02 19:23:04 +0000

    updated plotting of scores per token such that it breaks up the plot over lines)

--- lmdoctor/plot_utils.py
+++ lmdoctor/plot_utils.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
 import pandas as pd
 
@@ -38,34 +40,113 @@ def plot_projection_heatmap(all_projs, tokens, lastn_tokens_to_plot=None, satura
     )
     
     fig.show()
+
+
+# def plot_scores_per_token(
+#     readings, tokens, lastn_tokens_to_plot=None, detection_method=None, saturate_at=1, figsize=None, aspect=None, 
+#     token_range=None, hold_plot=False):
+#     """
+#     Scores (e.g. lie detection scores) per token.
+#     """
+#     plot_tokens = None
+
+#     if lastn_tokens_to_plot is not None and token_range is not None:
+#         raise RuntimeError('Cannot set both lastn_tokens_to_plot and token_range simultaneously.')
     
+#     if lastn_tokens_to_plot:
+#         plot_data = readings[:, -lastn_tokens_to_plot:]
+#         if tokens:
+#             plot_tokens = tokens[-lastn_tokens_to_plot:]
+#     elif token_range:
+#         plot_data = readings[:, token_range[0]:token_range[1]]
+#         if tokens:
+#             plot_tokens = tokens[token_range[0]:token_range[1]]
+#     else:
+#         plot_data = readings
+#         if tokens:
+#             plot_tokens = tokens
+
+#     fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"), aspect=aspect)
+
+#     if detection_method == 'classifier':
+#         fig.update_coloraxes(cmin=0, cmax=1)
+#     else:
+#         if saturate_at is not None:
+#             if saturate_at == -1:
+#                 # set the max and min based on largest value in data
+#                 absmax = np.max(np.abs(plot_data))
+#                 fig.update_coloraxes(cmin=-absmax, cmax=absmax)
+#             else:
+#                 fig.update_coloraxes(cmin=-saturate_at, cmax=saturate_at)
+
+
+#     if tokens:
+#         fig.update_xaxes(
+#             tickvals=list(range(len(plot_tokens))),
+#             ticktext=plot_tokens,
+#             tickangle=-45,  # Tilts the labels at -45 degrees
+#             tickfont=dict(size=20)  # Updates the font size to 12
+#         )
+
+#     fig.update_yaxes(
+#         showticklabels=False
+#     )
+
+#     fig.update_layout(coloraxis_showscale=False)
+
+#     if figsize:
+#         fig.update_layout(
+#             autosize=False,
+#             width=figsize[0],  # or any other width
+#             height=figsize[1]   # adjust the height accordingly
+#         )
+
+#     if hold_plot:
+#         return fig
+#     else:
+#         fig.show()
 
 
 def plot_scores_per_token(
-    readings, tokens, lastn_tokens_to_plot=None, detection_method=None, saturate_at=1, figsize=None, aspect=None, token_range=None):
+    readings, tokens, detection_method=None, saturate_at=1,
+    lastn_tokens_to_plot=None, token_ranges=None, auto_ranges_n=None,
+    figsize=None, aspect=None, tickangle=0, vertical_spacing=.1
+):
     """
     Scores (e.g. lie detection scores) per token.
     """
-    plot_tokens = None
+    num_not_none = int(lastn_tokens_to_plot is not None) + int(token_ranges is not None) + int(auto_ranges_n is not None)
 
-    if lastn_tokens_to_plot is not None and token_range is not None:
-        raise RuntimeError('Cannot set both lastn_tokens_to_plot and token_range simultaneously.')
+    if num_not_none > 1:
+        raise RuntimeError('Choose one of lastn_tokens_to_plot, token_ranges, and auto_ranges.')
+    elif num_not_none == 0:
+        token_ranges = [[0, len(readings[0])]]
+    elif lastn_tokens_to_plot:
+        token_ranges = [[-lastn_tokens_to_plot, len(readings[0])]]
+    elif auto_ranges_n:
+        n = auto_ranges_n
+        token_ranges = [(i, i+n) for i in range(0, len(readings[0]), n)]
+
+    rows = len(token_ranges)
+    fig = make_subplots(rows=rows, cols=1, shared_xaxes=False, vertical_spacing=vertical_spacing)
+    tokens_plus_idx = [f"{token}({idx})" for idx, token in enumerate(tokens)]
+
+    for i, tr in enumerate(token_ranges):
+        plot_tokens = None
+        plot_data = readings[:, tr[0]:tr[1]]
+        if tokens:
+            plot_tokens = tokens_plus_idx[tr[0]:tr[1]]
+
+        fig.add_trace(
+            go.Heatmap(
+                z=plot_data, 
+                x=plot_tokens,
+                colorscale='RdYlGn',  # Using the RdYlGn color scale
+                # coloraxis="coloraxis1"
+            ),
+            row=i+1, col=1
+        )
     
-    if lastn_tokens_to_plot:
-        plot_data = readings[:, -lastn_tokens_to_plot:]
-        if tokens:
-            plot_tokens = tokens[-lastn_tokens_to_plot:]
-    elif token_range:
-        plot_data = readings[:, token_range[0]:token_range[1]]
-        if tokens:
-            plot_tokens = tokens[token_range[0]:token_range[1]]
-    else:
-        plot_data = readings
-        if tokens:
-            plot_tokens = tokens
-
-    fig = px.imshow(plot_data, color_continuous_scale='RdYlGn', labels=dict(x="Token"), aspect=aspect)
-
     if detection_method == 'classifier':
         fig.update_coloraxes(cmin=0, cmax=1)
     else:
@@ -77,27 +158,24 @@ def plot_scores_per_token(
             else:
                 fig.update_coloraxes(cmin=-saturate_at, cmax=saturate_at)
 
-
     if tokens:
         fig.update_xaxes(
-            tickvals=list(range(len(plot_tokens))),
-            ticktext=plot_tokens,
-            tickangle=-45,  # Tilts the labels at -45 degrees
-            tickfont=dict(size=20)  # Updates the font size to 12
+            tickangle=tickangle, 
         )
 
     fig.update_yaxes(
         showticklabels=False
     )
 
+    # fig.update_layout(coloraxis_showscale=False)
 
     if figsize:
         fig.update_layout(
             autosize=False,
-            width=figsize[0],  # or any other width
-            height=figsize[1]   # adjust the height accordingly
+            width=figsize[0], 
+            height=figsize[1]  
         )
-        
+
     fig.show()
 
 
