62d3d21e4b753eccc7f5719d2af6053950c3d379
Josh Levy <joshlevy89@gmail.com>
2025-02-17 23:55:30 +0000

    v0.5.7; updated version and a few dependencies

--- lmdoctor/__init__.py
+++ lmdoctor/__init__.py
@@ -3,5 +3,5 @@ lmdoctor
 Extract, detect, and control representations within language models as they read and write text.
 """
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 __author__ = 'Josh Levy'
